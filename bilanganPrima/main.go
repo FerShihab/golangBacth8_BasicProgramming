3e87902c94ff106d66b625d6205b2f62f0c256fe
FShihab <fergiant.shihab.barrans.id>
2023-08-09 17:54:14 +0700

    delete comment

--- bilanganPrima/main.go
+++ bilanganPrima/main.go
@@ -5,13 +5,10 @@ import (
 )
 
 func isBilPrima(n int) bool {
-	// bilangan kurang dari atau sama dengan 1 bukan bilangan prima
 	if n <= 1 {
 		return false
 	}
 
-	// sistem looping untuk mengecek bilangan bila kondisi diatas tidak terpenuhi
-	// kondisi awal berada di int 2
 	for i := 2; i*i <= n; i++ {
 		if n%i == 0 {
 			return false
